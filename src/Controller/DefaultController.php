7108c908c6b97425a3812a09392dd2e9552cfe1c
musosy <musosy@users.noreply.github.com>
2021-05-27 09:53:58 +0200

    ^

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -11,7 +11,6 @@ class DefaultController extends AbstractController
     /**
      * @Route("/", name="app_index")
      */
-
     public function index(): Response
     {
         return $this->render('default/index.html.twig', [
