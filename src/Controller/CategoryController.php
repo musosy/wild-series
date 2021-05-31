eff790e16c1f99c82aa66001d33b12e5731cb9e7
musosy <musosy@users.noreply.github.com>
2021-05-31 16:37:54 +0200

    more display and functions

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Category;
 use App\Entity\Program;
+use App\Form\CategoryType;
 
 /**
  * @Route("/categories", name="category_")
@@ -28,6 +30,26 @@ class CategoryController extends AbstractController
             'categories' => $categories,
         ]);
     }
+
+    /**
+     * @Route("/new", name="new")
+     */
+    public function new(Request $request): Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($category);
+            $entityManager->flush();
+            return $this->redirectToRoute('category_index');
+        }
+        return $this->render('category/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
 
     /**
      * Show all related programs of a category
@@ -55,4 +77,6 @@ class CategoryController extends AbstractController
             'programs' => $programs
         ]);
     }
+
+    
 }
--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -4,10 +4,12 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Program;
 use App\Entity\Season;
 use App\Entity\Episode;
+use App\Form\ProgramType;
 
 /**
  * @Route("/programs", name="program_")
@@ -35,18 +37,32 @@ class ProgramController extends AbstractController
     }
 
     /**
+     * @Route("/new", name="new")
+     */
+    public function new(Request $request): Response
+    {
+        $program = new Program();
+        $form = $this->createForm(ProgramType::class, $program);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($program);
+            $entityManager->flush();
+            return $this->redirectToRoute('program_index');
+        }
+        return $this->render('program/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
      * Getting a program by id
      * 
-     * @Route("/{id<^[0-9]+$>}", methods={"GET"}, name="show")
+     * @Route("/{program<^[0-9]+$>}", methods={"GET"}, name="show")
      * @return Response
      */
     public function show(Program $program): Response
     {
-        if (!$program) {
-            throw $this->createNotFoundException(
-                'No program found in program\'s table.'
-            );
-        }
         $seasons = $program->getSeasons();
         return $this->render(
             'program/show.html.twig',
@@ -59,13 +75,13 @@ class ProgramController extends AbstractController
     /**
      * Get a specific season of a program
      * 
-     * @Route("/{id<^[0-9]+$>}/season/{seasonId<^[0-9]+$>}", methods={"GET"}, name="season_show")
+     * @Route("/{program<^[0-9]+$>}/season/{season<^[0-9]+$>}", methods={"GET"}, name="season_show")
      * @return Response
      */
     public function showSeason(Program $program, Season $season)
     {
         $episodes = $season->getEpisodes();
-        return $this->render('program/show_season.html.twig', [
+        return $this->render('program/season_show.html.twig', [
             'program' => $program,
             'season' => $season,
             'episodes' => $episodes
@@ -75,15 +91,30 @@ class ProgramController extends AbstractController
     /**
      * Get a specific episode of a program
      * 
-     * @Route("/{id<^[0-9]+$>}/season/{seasonId<^[0-9]+$>}/episode/{episodeId<^[0-9]+$>}", methods={"GET"}, name="episode_show")
+     * @Route("/{program<^[0-9]+$>}/season/{season<^[0-9]+$>}/episode/{episode<^[0-9]+$>}", methods={"GET"}, name="episode_show")
      * @return Response
      */
     public function showEpisode(Program $program, Season $season, Episode $episode)
     {
+        $prev = null;
+        if ($episode->getId() - 1 > 0) {
+            $prev = $this->getDoctrine()
+                ->getRepository(Episode::class)
+                ->findOneBy([
+                    'id' => ($episode->getId() - 1)
+                ]);
+        }
+        $next = $this->getDoctrine()
+            ->getRepository(Episode::class)
+            ->findOneBy([
+                'id' => ($episode->getId() + 1)
+            ]);
         return $this->render('program/episode_show.html.twig', [
             'program' => $program,
             'season' => $season,
-            'episode' => $episode
+            'episode' => $episode,
+            'prev' => $prev,
+            'next' => $next
         ]);
     }
 }
